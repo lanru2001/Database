dd99be54cd8ec4efde4322615579b7e720fad161
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-03-21 20:54:49 -0500

    Update module.tf

--- module.tf
+++ module.tf
@@ -8,13 +8,13 @@ terraform {
 
 variable "name" {
   type        = string
-  default     = "rds-postgres"
+  default     = "aurora-sls-pg"
   description = "The name of the module"
 }
 
 variable terraform_module {
   type        = string
-  default     = "gravicore/terraform-gravicore-modules/aws/rds-postgres"
+  default     = "gravicore/terraform-gravicore-modules/aws/aurora-sls-pg"
   description = "The owner and name of the Terraform module"
 }
 

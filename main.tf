268d300fe8be76d43ecdc3c25b1bd2f632565bee
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-07-13 19:40:10 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,12 +30,6 @@ variable "create" {
   description = "Set to false to prevent the module from creating any resources"
 }
 
-# ----------------------------------------------------------------------------------------------------------------------
-# Platform Standard Variables
-# ----------------------------------------------------------------------------------------------------------------------
-
-# Recommended
-
 variable "namespace" {
   type        = string
   default     = ""
@@ -71,8 +65,6 @@ variable "master_account_id" {
   default     = ""
   description = "The Master AWS Account ID that owns the associate AWS account"
 }
-
-# Optional
 
 variable "tags" {
   type        = map(string)
@@ -110,10 +102,261 @@ variable "delimiter" {
   description = "Delimiter to be used between `namespace`, `environment`, `stage`, `name`"
 }
 
-# Derived
+variable "zone_id" {
+  type        = string
+  default     = ""
+  description = "Route53 parent zone ID. If provided (not empty), the module will create sub-domain DNS records for the DB master and replicas"
+}
 
-data "aws_caller_identity" "current" {
-  count = var.account_id == "" ? 1 : 0
+variable "security_groups" {
+  type        = list(string)
+  default     = []
+  description = "List of security groups to be allowed to connect to the DB instance"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID to create the cluster in (e.g. `vpc-a22222ee`)"
+}
+
+variable "subnets" {
+  type        = list(string)
+  description = "List of VPC subnet IDs"
+}
+
+variable "db_subnet_group_name" {
+  type        = string
+  default     = null
+  description = "(Optional) A DB subnet group to associate with this DB instance. NOTE: This must match the db_subnet_group_name specified on every aws_rds_cluster_instance in the cluster"
+}
+
+variable "db_cluster_parameter_group_name" {
+  type        = string
+  default     = null
+  description = "(Optional) The name of the DB parameter group to associate with this instance."
+}
+
+variable "cluster_identifier" {
+  type        = string
+  default     = ""
+  description = "The RDS Cluster Identifier. Will use generated label ID if not supplied"
+}
+
+variable "snapshot_identifier" {
+  type        = string
+  default     = ""
+  description = "Specifies whether or not to create this cluster from a snapshot"
+}
+
+variable "db_name" {
+  type        = string
+  default     = ""
+  description = "Database name (default is not to create a database)"
+}
+
+variable "db_port" {
+  type        = number
+  default     = 5432
+  description = "Database port"
+}
+
+variable "admin_user" {
+  type        = string
+  default     = ""
+  description = "(Required unless a snapshot_identifier is provided) Username for the master DB user"
+}
+
+variable "admin_password" {
+  type        = string
+  default     = ""
+  description = "(Required unless a snapshot_identifier is provided) Password for the master DB user"
+}
+
+variable "retention_period" {
+  type        = number
+  default     = 7
+  description = "Number of days to retain backups for"
+}
+
+variable "backup_window" {
+  type        = string
+  default     = "07:55-08:25"
+  description = "Daily time range during which the backups happen"
+}
+
+variable "maintenance_window" {
+  type        = string
+  default     = "sat:03:00-sat:04:00"
+  description = "Weekly time range during which system maintenance can occur, in UTC"
+}
+
+variable "cluster_parameters" {
+  type = list(object({
+    apply_method = string
+    name         = string
+    value        = string
+  }))
+  default     = []
+  description = "List of DB cluster parameters to apply"
+}
+
+variable "instance_parameters" {
+  type = list(object({
+    apply_method = string
+    name         = string
+    value        = string
+  }))
+  default     = []
+  description = "List of DB instance parameters to apply"
+}
+
+variable "cluster_family" {
+  type        = string
+  default     = "aurora-postgresql10"
+  description = "The family of the DB cluster parameter group"
+}
+
+variable "engine" {
+  type        = string
+  default     = "aurora-postgresql"
+  description = "The name of the database engine to be used for this DB cluster. Valid values: `aurora`, `aurora-mysql`, `aurora-postgresql`"
+}
+
+variable "engine_mode" {
+  type        = string
+  default     = "serverless"
+  description = "The database engine mode. Valid values: `parallelquery`, `provisioned`, `serverless`"
+}
+
+variable "engine_version" {
+  type        = string
+  default     = ""
+  description = "The version of the database engine to use. See `aws rds describe-db-engine-versions` "
+}
+
+variable "scaling_configuration" {
+  type = list(object({
+    auto_pause               = bool
+    max_capacity             = number
+    min_capacity             = number
+    seconds_until_auto_pause = number
+    timeout_action           = string
+  }))
+  default     = []
+  description = "List of nested attributes with scaling properties. Only valid when `engine_mode` is set to `serverless`"
+}
+
+variable "timeouts_configuration" {
+  type = list(object({
+    create = string
+    update = string
+    delete = string
+  }))
+  default     = []
+  description = "List of timeout values per action. Only valid actions are `create`, `update` and `delete`"
+}
+
+variable "allowed_cidr_blocks" {
+  type        = list(string)
+  default     = []
+  description = "List of CIDR blocks allowed to access the cluster"
+}
+
+variable "storage_encrypted" {
+  type        = bool
+  description = "Specifies whether the DB cluster is encrypted. The default is `false` for `provisioned` `engine_mode` and `true` for `serverless` `engine_mode`"
+  default     = false
+}
+
+variable "kms_key_arn" {
+  type        = string
+  description = "The ARN for the KMS encryption key. When specifying `kms_key_arn`, `storage_encrypted` needs to be set to `true`"
+  default     = ""
+}
+
+variable "skip_final_snapshot" {
+  type        = bool
+  description = "Determines whether a final DB snapshot is created before the DB cluster is deleted"
+  default     = true
+}
+
+variable "copy_tags_to_snapshot" {
+  type        = bool
+  description = "Copy tags to backup snapshots"
+  default     = false
+}
+
+variable "deletion_protection" {
+  type        = bool
+  description = "If the DB instance should have deletion protection enabled"
+  default     = false
+}
+
+variable "apply_immediately" {
+  type        = bool
+  description = "Specifies whether any cluster modifications are applied immediately, or during the next maintenance window"
+  default     = true
+}
+
+variable "iam_database_authentication_enabled" {
+  type        = bool
+  description = "Specifies whether or mappings of AWS Identity and Access Management (IAM) accounts to database accounts is enabled"
+  default     = false
+}
+
+variable "replication_source_identifier" {
+  type        = string
+  description = "ARN of a source DB cluster or DB instance if this DB cluster is to be created as a Read Replica"
+  default     = ""
+}
+
+variable "enabled_cloudwatch_logs_exports" {
+  type        = list(string)
+  description = "List of log types to export to cloudwatch. The following log types are supported: audit, error, general, slowquery"
+  default     = []
+}
+
+variable "cluster_dns_name" {
+  type        = string
+  description = "Name of the cluster CNAME record to create in the parent DNS zone specified by `zone_id`. If left empty, the name will be auto-asigned using the format `master.var.name`"
+  default     = ""
+}
+
+variable "global_cluster_identifier" {
+  type        = string
+  description = "ID of the Aurora global cluster"
+  default     = ""
+}
+
+variable "source_region" {
+  type        = string
+  description = "Source Region of primary cluster, needed when using encrypted storage and region replicas"
+  default     = ""
+}
+
+variable "iam_roles" {
+  type        = list(string)
+  description = "Iam roles for the Aurora cluster"
+  default     = []
+}
+
+variable "backtrack_window" {
+  type        = number
+  description = "The target backtrack window, in seconds. Only available for aurora engine currently. Must be between 0 and 259200 (72 hours)"
+  default     = 0
+}
+
+variable "enable_http_endpoint" {
+  type        = bool
+  description = "Enable HTTP endpoint (data API). Only valid when engine_mode is set to serverless"
+  default     = false
+}
+
+variable "vpc_security_group_ids" {
+  type        = list(string)
+  description = "Additional security group IDs to apply to the cluster, in addition to the provisioned default security group with ingress traffic from existing CIDR blocks and existing security groups"
+
+  default = []
 }
 
 locals {
